f0f5b8b9bbc3e3733652328735fb4cbd6c368884
ECTheHunter <emirhancatalbas@windowslive.com>
2023-04-09 00:14:55 +0300

    .

--- gamejam/Assets/Scripts/Movement.cs
+++ gamejam/Assets/Scripts/Movement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -155,6 +156,13 @@ public class Movement : MonoBehaviour
             }
         }
         
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "LevelEnd")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
     public void Throw()
     {
